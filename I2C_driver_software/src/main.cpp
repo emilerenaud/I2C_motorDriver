109fe0fbcd202b37c54c67628391d2c05e72a73c
MachineJDG <mimil2014@gmail.com>
2023-10-04 22:42:03 -0400

    start soft

---
+++ I2C_driver_software/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define motorPinA PD6
+#define motorPinB PD5
+#define encoderPinA PD4
+#define encoderPinB PD3
+
+#define led1 PB0
+#define led2 PB1
+
+#define SDA PC4
+#define SCL PC5
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
